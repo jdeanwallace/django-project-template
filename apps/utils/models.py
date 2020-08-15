fa8d22bfb835e7c2fbe60a33c6f9d7f03d6b7bce
Jason Wallace <jdeanwallace@gmail.com>
2020-08-15 16:55:58 +0200

    Update & test JSON model fields.

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -1,14 +1,10 @@
 import json
 
+from django import forms
 from django.db import models
+from django.core import exceptions
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.postgres.fields import JSONField
-from django.contrib.postgres.forms.jsonb import (
-    JSONField as JSONFormField, InvalidJSONInput,
-)
-from django.core.serializers.json import DjangoJSONEncoder
-from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -17,139 +13,126 @@ class CreatedModifiedModel(models.Model):
     modified_at = models.DateTimeField(default=timezone.now)
 
     def save(self, *args, **kwargs):
-        self.modified_at = timezone.now()
+        if not self.pk:
+            self.modified_at = self.created_at
+        else:
+            self.modified_at = timezone.now()
         super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
 
 
-class UpdateModelMixin(object):
+class JSONFormField(forms.JSONField):
 
-    def update(self, _force_save=False, **kwargs):
-        modified = False
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value() if callable(value) else value)
-                modified = True
+    def __init__(self, empty_value=None, **kwargs):
+        super().__init__(**{'empty_value': None, **kwargs})
 
-        if modified or _force_save:
-            self.save()
+    def is_valid(self, value):
+        return isinstance(value, (list, dict, int, float, forms.JSONString))
+
+    def to_python(self, value):
+        if self.disabled:
+            return value
+        try:
+            # Try load JSON from string
+            converted = json.loads(value, cls=self.decoder)
+            if isinstance(converted, str):
+                converted = forms.JSONString(converted)
+        except json.JSONDecodeError:
+            # Invalid JSON
+            converted = forms.fields.InvalidJSONInput(value)
+        except TypeError:
+            # Not a string
+            converted = value
+        if converted in self.empty_values:
+            return self.empty_value
+        elif self.is_valid(converted):
+            return converted
+        raise exceptions.ValidationError(
+            self.error_messages['invalid'],
+            code='invalid',
+            params={'value': value},
+        )
+
+    def bound_data(self, data, initial):
+        if self.disabled:
+            return initial
+        try:
+            # Try load JSON from string
+            value = json.loads(data, cls=self.decoder)
+            if not self.is_valid(value):
+                raise ValueError(value)
+        except (json.JSONDecodeError, ValueError):
+            return forms.fields.InvalidJSONInput(data)
+        else:
+            return value
 
 
 class JSONObjectFormField(JSONFormField):
     default_error_messages = {
-        'invalid': _("'%(value)s' value must be valid JSON object."),
+        'invalid': _("'%(value)s' value must be a valid JSON object."),
     }
-    empty_values = [None, '', [], ()]
+    empty_values = [None, '']
 
-    def to_python(self, value):
-        if value in self.empty_values:
-            return None
-        try:
-            python_value = json.loads(value)
-            if not isinstance(python_value, (dict,)):
-                raise ValueError()
-            return python_value
-        except ValueError:
-            raise ValidationError(
-                self.error_messages['invalid'],
-                code='invalid',
-                params={'value': value},
-            )
-
-    def bound_data(self, data, initial):
-        if self.disabled:
-            return initial
-        try:
-            bound_data = json.loads(data)
-            if not isinstance(bound_data, (dict,)):
-                raise ValueError()
-            return bound_data
-        except ValueError:
-            return InvalidJSONInput(data)
+    def is_valid(self, value):
+        return isinstance(value, (dict,))
 
 
 class JSONArrayFormField(JSONFormField):
     default_error_messages = {
-        'invalid': _("'%(value)s' value must be valid JSON array."),
+        'invalid': _("'%(value)s' value must be a valid JSON array."),
     }
-    empty_values = [None, '', {}, ()]
+    empty_values = [None, '']
 
-    def to_python(self, value):
-        if value in self.empty_values:
-            return None
-        try:
-            python_value = json.loads(value)
-            if not isinstance(python_value, (list,)):
-                raise ValueError()
-            return python_value
-        except ValueError:
-            raise ValidationError(
-                self.error_messages['invalid'],
-                code='invalid',
-                params={'value': value},
-            )
-
-    def bound_data(self, data, initial):
-        if self.disabled:
-            return initial
-        try:
-            bound_data = json.loads(data)
-            if not isinstance(bound_data, (list,)):
-                raise ValueError()
-            return bound_data
-        except ValueError:
-            return InvalidJSONInput(data)
+    def is_valid(self, value):
+        return isinstance(value, (list,))
 
 
-class JSONObjectField(JSONField):
+class JSONObjectField(models.JSONField):
     description = _('A JSON object')
     default_error_messages = {
         'invalid': _("Value must be a valid JSON object."),
     }
-    empty_values = [None, '', [], ()]
-
-    def __init__(self, verbose_name=None, name=None, encoder=DjangoJSONEncoder,
-                 **kwargs):
-        super().__init__(verbose_name, name, encoder, **kwargs)
+    empty_values = [None, '']
 
     def validate(self, value, model_instance):
-        if not isinstance(value, (dict,)):
-            raise ValidationError(
+        super().validate(value, model_instance)
+        if value is not None and not isinstance(value, (dict,)):
+            raise exceptions.ValidationError(
                 self.error_messages['invalid'],
                 code='invalid',
                 params={'value': value},
             )
-        super().validate(value, model_instance)
+
 
     def formfield(self, **kwargs):
-        defaults = {'form_class': JSONObjectFormField}
-        defaults.update(kwargs)
-        return super().formfield(**defaults)
+        return super().formfield(**{
+            'form_class': JSONObjectFormField,
+            'required': not self.blank or not self.null,
+            **kwargs,
+        })
 
 
-class JSONArrayField(JSONField):
+class JSONArrayField(models.JSONField):
     description = _('A JSON array')
     default_error_messages = {
         'invalid': _("Value must be a valid JSON array."),
     }
-    empty_values = [None, '', {}, ()]
-
-    def __init__(self, verbose_name=None, name=None, encoder=DjangoJSONEncoder,
-                 **kwargs):
-        super().__init__(verbose_name, name, encoder, **kwargs)
+    empty_values = [None, '']
 
     def validate(self, value, model_instance):
-        if not isinstance(value, (list,)):
-            raise ValidationError(
+        super().validate(value, model_instance)
+        if value is not None and not isinstance(value, (list,)):
+            raise exceptions.ValidationError(
                 self.error_messages['invalid'],
                 code='invalid',
                 params={'value': value},
             )
-        super().validate(value, model_instance)
 
     def formfield(self, **kwargs):
-        defaults = {'form_class': JSONArrayFormField}
-        defaults.update(kwargs)
-        return super().formfield(**defaults)
+        return super().formfield(**{
+            'form_class': JSONArrayFormField,
+            'required': not self.blank or not self.null,
+            **kwargs,
+        })
