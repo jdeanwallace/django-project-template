b7812c722eb78076953f51cc37118da0410ff3a8
Jason Wallace <jdeanwallace@gmail.com>
2023-09-23 17:52:49 +0200

    Drop postgres database in prod.

--- example/settings/prod.py
+++ example/settings/prod.py
@@ -5,18 +5,3 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
-
-
-# Database
-# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DEFAULT_DATABASE_NAME'),
-        'USER': os.environ.get('DEFAULT_DATABASE_USER'),
-        'PASSWORD': os.environ.get('DEFAULT_DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DEFAULT_DATABASE_HOST'),
-        'PORT': os.environ.get('DEFAULT_DATABASE_PORT'),
-    }
-}
