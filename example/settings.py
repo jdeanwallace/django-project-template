c0817a17ff130389550df6effdea4f4b859b1175
Jason Wallace <jdeanwallace@gmail.com>
2024-01-25 11:08:59 +0200

    Use dummy secret key.

--- example/settings.py
+++ example/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jl%97dh#yx*=^47icvx$a@@6o(92)@&j7^wwkmz#g=xlp%-v)6'
+SECRET_KEY = 'dummy-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
