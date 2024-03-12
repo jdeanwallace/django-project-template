1cd3570f2216270f856d87b011dafd36a9e25739
Jason Wallace <jdeanwallace@gmail.com>
2024-03-12 22:15:51 +0200

    Add django-ses and stuff.

--- example/settings/prod.py
+++ example/settings/prod.py
@@ -6,4 +6,16 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "example.fly.dev",
+    "example.com",
 ]
+
+EMAIL_BACKEND = "django_ses.SESBackend"
+
+AWS_SES_ACCESS_KEY_ID = os.environ.get("AWS_SES_ACCESS_KEY_ID", "")
+AWS_SES_SECRET_ACCESS_KEY = os.environ.get("AWS_SES_SECRET_ACCESS_KEY", "")
+AWS_SES_REGION_NAME = os.environ.get("AWS_SES_REGION_NAME", "")
+AWS_SES_REGION_ENDPOINT = os.environ.get("AWS_SES_REGION_ENDPOINT", "")
+
+SERVER_EMAIL = "Example Server <server@example.com>"
+DEFAULT_FROM_EMAIL = "Admin from Example <admin@example.com>"
+ADMINS = [("Admin", "admin@example.com")]
